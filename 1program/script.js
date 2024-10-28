df9673421123e2dfd68a7a707516fc609ea41084
Sachin Maurya <kumarsachin67671@gmail.com>
2024-10-28 14:53:37 +0530

    hover mouse

--- 1program/script.js
+++ 1program/script.js
@@ -1,13 +1,13 @@
 // Function to handle color button clicks
-function colorButtonHandler(color) {
+function color(color) {
     const messageElement = document.getElementById('message');
     messageElement.textContent = `You chose the color: ${color}`;
     document.body.style.backgroundColor = `${color}`; // Green
 }
 
 // Assign event handlers to buttons
-document.getElementById('red').onclick = function() { colorButtonHandler('Red'); };
-document.getElementById('blue').onclick = function() { colorButtonHandler('Blue'); };
-document.getElementById('yellow').onclick = function() { colorButtonHandler('Yellow'); };
-document.getElementById('green').onclick = function() { colorButtonHandler('Green'); };
-document.getElementById('orange').onclick = function() { colorButtonHandler('Orange'); };
+document.getElementById('red').onclick = function() { color('Red'); };
+document.getElementById('blue').onclick = function() { color('Blue'); };
+document.getElementById('yellow').onclick = function() { color('Yellow'); };
+document.getElementById('green').onclick = function() { color('Green'); };
+document.getElementById('orange').onclick = function() { color('Orange'); };
